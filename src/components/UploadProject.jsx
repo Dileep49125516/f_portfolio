dd55cd42194e58e0d2bff4cf9f35380846b26212
Dileep kumar <burladileepkumar13@gmail.com>
2024-09-03 15:52:40 +0530

    changes happen

--- src/components/UploadProject.jsx
+++ src/components/UploadProject.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-const API_URL = import.meta.env.VITE_API_URL;
+import { API_URL } from './data/apipath';
+
 const UploadProject = ({ setShowProjects,setShowHeader}) => {
   const [title, setTitle] = useState("");
   const [technicalSkills, setTechnicalSkills] = useState("");
---
+++ src/components/data/apipath.js
@@ -0,0 +1,1 @@
+export const API_URL="https://b-mydetails.onrender.com"
